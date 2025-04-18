15b76f805eed0379d37b9b2689889047fca0ee2a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-18 14:45:55 +0200

    feat(UX): Writing the initial WebApplication

---
+++ tutor-web-app-ssr/src/bin/iter5-ssr.rs
@@ -0,0 +1,40 @@
+#[path = "../iter5/mod.rs"]
+mod iter5;
+use std::env;
+
+use actix_web::{
+    App, HttpServer,
+    web::{self, Data},
+};
+use dotenv::dotenv;
+use iter5::{routes::app_config, state::AppState};
+use sqlx::PgPool;
+use tera::Tera;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    // * INFO: Start HTTP Server
+    let host_port = env::var("HOST_PORT").expect("HOST:HOST_PORT is not set in .env file");
+    println!("Listenig on http://{}", &host_port);
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+
+    // * INFO: Contruct APP State
+    let shared_data = web::Data::new(AppState {
+        db: db_pool.clone(),
+    });
+
+    HttpServer::new(move || {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/static/iter5/**/*")).unwrap();
+
+        App::new()
+            .app_data(Data::new(tera))
+            .app_data(shared_data.clone())
+            .configure(app_config)
+    })
+    .bind(&host_port)?
+    .run()
+    .await
+}
---
+++ tutor-web-app-ssr/src/iter5/handler.rs
@@ -0,0 +1,12 @@
+use actix_web::{Error, HttpResponse};
+
+// __ * INFO: Handler function to show the registration form to the user __
+pub async fn show_register_form() -> Result<HttpResponse, Error> {
+    let msg = "Hello, you are in the registration page.";
+
+    Ok(HttpResponse::Ok().content_type("text/html").body(msg))
+}
+
+pub async fn handle_register() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().content_type("text/html").body(""))
+}
