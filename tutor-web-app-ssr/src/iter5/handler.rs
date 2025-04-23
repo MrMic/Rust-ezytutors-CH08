64b3f34acbd7eb1516d0ef1e5fa50311fd9d253e
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-23 15:07:07 +0200

    feat(UX/UI): Display the Registration Form

--- tutor-web-app-ssr/src/iter5/handler.rs
+++ tutor-web-app-ssr/src/iter5/handler.rs
@@ -1,10 +1,22 @@
-use actix_web::{Error, HttpResponse};
+use actix_web::{Error, HttpResponse, web};
+
+use super::errors::EzyTutorError;
 
 // __ * INFO: Handler function to show the registration form to the user __
-pub async fn show_register_form() -> Result<HttpResponse, Error> {
-    let msg = "Hello, you are in the registration page.";
+pub async fn show_register_form(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let mut ctx = tera::Context::new();
+    ctx.insert("error", "");
+    ctx.insert("current_username", "");
+    ctx.insert("current_password", "");
+    ctx.insert("current_confirmation", "");
+    ctx.insert("current_name", "");
+    ctx.insert("current_imageurl", "");
+    ctx.insert("current_profile", "");
+    let s = tmpl
+        .render("register.html", &ctx)
+        .map_err(|_| EzyTutorError::TeraError("Template Error".to_string()))?;
 
-    Ok(HttpResponse::Ok().content_type("text/html").body(msg))
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
 pub async fn handle_register() -> Result<HttpResponse, Error> {
